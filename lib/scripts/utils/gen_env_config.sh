bf012349e17684c05dd21ce5c6613750fd6c19f3
QuikAppTech <prasannasrie@gmail.com>
2025-06-18 23:33:05 +0530

    android build 12

--- lib/scripts/utils/gen_env_config.sh
+++ lib/scripts/utils/gen_env_config.sh
@@ -8,12 +8,12 @@ dart_bool() {
 
 cat > lib/config/env_config.dart <<EOF
 class EnvConfig {
-  static const String appName = '${APP_NAME:-''}';
-  static const String orgName = '${ORG_NAME:-''}';
-  static const String webUrl = '${WEB_URL:-''}';
-  static const String pkgName = '${PKG_NAME:-''}';
-  static const String bundleId = '${BUNDLE_ID:-''}';
-  static const String emailId = '${EMAIL_ID:-''}';
+  static const String appName = "${APP_NAME:-}";
+  static const String orgName = "${ORG_NAME:-}";
+  static const String webUrl = "${WEB_URL:-}";
+  static const String pkgName = "${PKG_NAME:-}";
+  static const String bundleId = "${BUNDLE_ID:-}";
+  static const String emailId = "${EMAIL_ID:-}";
   static const bool isChatbot = $(dart_bool "${IS_CHATBOT:-false}");
   static const bool isDomainUrl = $(dart_bool "${IS_DOMAIN_URL:-false}");
   static const bool isSplash = $(dart_bool "${IS_SPLASH:-false}");
@@ -28,27 +28,27 @@ class EnvConfig {
   static const bool isBiometric = $(dart_bool "${IS_BIOMETRIC:-false}");
   static const bool isCalendar = $(dart_bool "${IS_CALENDAR:-false}");
   static const bool isStorage = $(dart_bool "${IS_STORAGE:-false}");
-  static const String logoUrl = '${LOGO_URL:-''}';
-  static const String splashUrl = '${SPLASH_URL:-''}';
-  static const String splashBg = '${SPLASH_BG_URL:-''}';
-  static const String splashBgColor = '${SPLASH_BG_COLOR:-''}';
-  static const String splashTagline = '${SPLASH_TAGLINE:-''}';
-  static const String splashTaglineColor = '${SPLASH_TAGLINE_COLOR:-''}';
-  static const String splashAnimation = '${SPLASH_ANIMATION:-''}';
+  static const String logoUrl = "${LOGO_URL:-}";
+  static const String splashUrl = "${SPLASH_URL:-}";
+  static const String splashBg = "${SPLASH_BG_URL:-}";
+  static const String splashBgColor = "${SPLASH_BG_COLOR:-}";
+  static const String splashTagline = "${SPLASH_TAGLINE:-}";
+  static const String splashTaglineColor = "${SPLASH_TAGLINE_COLOR:-}";
+  static const String splashAnimation = "${SPLASH_ANIMATION:-}";
   static const int splashDuration = ${SPLASH_DURATION:-0};
   static const String bottommenuItems = r"""${BOTTOMMENU_ITEMS:-[]}""";
-  static const String bottommenuBgColor = '${BOTTOMMENU_BG_COLOR:-''}';
-  static const String bottommenuIconColor = '${BOTTOMMENU_ICON_COLOR:-''}';
-  static const String bottommenuTextColor = '${BOTTOMMENU_TEXT_COLOR:-''}';
-  static const String bottommenuFont = '${BOTTOMMENU_FONT:-''}';
+  static const String bottommenuBgColor = "${BOTTOMMENU_BG_COLOR:-}";
+  static const String bottommenuIconColor = "${BOTTOMMENU_ICON_COLOR:-}";
+  static const String bottommenuTextColor = "${BOTTOMMENU_TEXT_COLOR:-}";
+  static const String bottommenuFont = "${BOTTOMMENU_FONT:-}";
   static const double bottommenuFontSize = ${BOTTOMMENU_FONT_SIZE:-0};
   static const bool bottommenuFontBold = $(dart_bool "${BOTTOMMENU_FONT_BOLD:-false}");
   static const bool bottommenuFontItalic = $(dart_bool "${BOTTOMMENU_FONT_ITALIC:-false}");
-  static const String bottommenuActiveTabColor = '${BOTTOMMENU_ACTIVE_TAB_COLOR:-''}';
-  static const String bottommenuIconPosition = '${BOTTOMMENU_ICON_POSITION:-''}';
-  static const String bottommenuVisibleOn = '${BOTTOMMENU_VISIBLE_ON:-''}';
+  static const String bottommenuActiveTabColor = "${BOTTOMMENU_ACTIVE_TAB_COLOR:-}";
+  static const String bottommenuIconPosition = "${BOTTOMMENU_ICON_POSITION:-}";
+  static const String bottommenuVisibleOn = "${BOTTOMMENU_VISIBLE_ON:-}";
   static const bool pushNotify = $(dart_bool "${PUSH_NOTIFY:-false}");
-  static const String firebaseConfigAndroid = '${FIREBASE_CONFIG_ANDROID:-''}';
-  static const String firebaseConfigIos = '${FIREBASE_CONFIG_IOS:-''}';
+  static const String firebaseConfigAndroid = "${FIREBASE_CONFIG_ANDROID:-}";
+  static const String firebaseConfigIos = "${FIREBASE_CONFIG_IOS:-}";
 }
 EOF
